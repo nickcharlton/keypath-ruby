a53e96aa1914dd3c96b21e826e5c84a6ccb295e1
Nick Charlton <hello@nickcharlton.net>
2013-12-17 17:07:04 +0000

    Fixes file references and typos in the spec.

--- spec/key_path_spec.rb
+++ spec/key_path_spec.rb
@@ -2,14 +2,14 @@
 require File.expand_path 'spec_helper.rb', __dir__
 
 # the files we're specifically testing
-require File.expand_path '../lib/key_path.rb', __dir__
+require File.expand_path '../lib/key_path/key_path.rb', __dir__
 
 describe 'KeyPath' do
   it 'creates a KeyPath instance from a string' do
-    path = KeyPath.new('item.url')
+    path = KeyPath::Path.new('item.url')
 
     path.wont_be_nil
     path.to_s.must_be_kind_of String
-    path.to_s.must_be 'item.url'
+    path.to_s.must_equal 'item.url'
   end
 end
