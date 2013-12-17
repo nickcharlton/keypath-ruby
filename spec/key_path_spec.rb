e0377b3f07d40a93c1b578be304ada5a8bcd68f3
Nick Charlton <hello@nickcharlton.net>
2013-12-17 17:21:49 +0000

    Adds specs for the inspect, parent and output methods.

--- spec/key_path_spec.rb
+++ spec/key_path_spec.rb
@@ -12,4 +12,31 @@ describe 'KeyPath' do
     path.to_s.must_be_kind_of String
     path.to_s.must_equal 'item.url'
   end
+
+  it 'outputs an array of items in the path' do
+    path = KeyPath::Path.new('item.url')
+
+    path.to_a.must_be_kind_of Array
+    path.to_a.must_equal ['item', 'url']
+  end
+
+  it 'spits out a useful inspect string' do
+    path = KeyPath::Path.new('item.url')
+
+    path.inspect.must_be_kind_of String
+    path.inspect.wont_be_nil
+  end
+
+  it 'returns a parent if one exists' do
+    path = KeyPath::Path.new('item.url')
+
+    path.parent.wont_be_nil
+    path.parent.must_be_kind_of KeyPath::Path
+  end
+
+  it 'returns nil if a parent doesnt exist' do
+    path = KeyPath::Path.new('item')
+
+    path.parent.must_be_nil
+  end
 end
