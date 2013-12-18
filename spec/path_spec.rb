f3bc0c69ace5b6b489c3ddcfa8bd1a0c6bfc06b1
Nick Charlton <hello@nickcharlton.net>
2013-12-18 14:36:59 +0000

    Adds a spec for the collections generation.

--- spec/path_spec.rb
+++ spec/path_spec.rb
@@ -1,7 +1,7 @@
 # test helpers
 require File.expand_path 'spec_helper.rb', __dir__
 
-describe 'KeyPath::Path' do
+describe 'KeyPath::Path main methods' do
   it 'creates a KeyPath instance from a string' do
     path = KeyPath::Path.new('item.url')
 
@@ -35,6 +35,33 @@ describe 'KeyPath::Path' do
     path = KeyPath::Path.new('item')
 
     path.parent.must_be_nil
+  end
+end
+
+describe 'KeyPath::Path collections generation' do
+  it 'returns an empty hash with an empty path' do
+    path = KeyPath::Path.new('')
+    path.to_collection.must_equal({})
+  end
+
+  it 'returns a nested hash for a single path unit' do
+    path = KeyPath::Path.new('item')
+    path.to_collection.must_equal({:item => {}})
+  end
+
+  it 'returns a nested array when the key is plural' do
+    path = KeyPath::Path.new('items')
+    path.to_collection.must_equal({:items => []})
+  end
+
+  it 'returns a double nested array with a two set keypath' do
+    path = KeyPath::Path.new('item.id')
+    path.to_collection.must_equal({:item => {:id => {}}})
+  end
+
+  it 'returns a nested array with an item' do
+    path = KeyPath::Path.new('items.0.id')
+    path.to_collection.must_equal({:items => [{:id => {}}]})
   end
 end
 
