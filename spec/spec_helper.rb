c46bdd928a4fc72bc6ee13c3ca1c0b0f3aacb027
Nick Charlton <hello@nickcharlton.net>
2013-12-18 17:05:13 +0000

    Adds Coveralls support.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'coveralls'
+
+# enable coveralls
+Coveralls.wear!
 
 # pull in the library
 require File.expand_path '../lib/key_path.rb', __dir__
