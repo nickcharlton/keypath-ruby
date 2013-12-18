b917e78c1368dff9d72532799bc01f22f1a31550
Nick Charlton <hello@nickcharlton.net>
2013-12-18 11:00:22 +0000

    Gives string extensions the same structure as Hash.

--- lib/key_path.rb
+++ lib/key_path.rb
@@ -5,7 +5,7 @@ require 'key_path/path'
 # extensions
 require 'key_path/hash/deep_merge'
 require 'key_path/hash/extensions'
-require 'key_path/string_extensions'
+require 'key_path/string/extensions'
 
 
 module KeyPath
