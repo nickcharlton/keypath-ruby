b124a4b682494b5030ee64f9363d49ce7fe41fc2
Nick Charlton <hello@nickcharlton.net>
2013-12-18 10:57:45 +0000

    Adds the Hash extension and spec.

---
+++ lib/key_path/hash/extensions.rb
@@ -0,0 +1,31 @@
+module KeyPath
+  module HashExtensions
+    def keypaths_for_nested_key(nested_key = '', nested_hash=self, path=[], all_values={})
+      nested_hash.each do |k, v|
+        path << k.to_s # assemble the path from the key
+        case v
+        when String, Fixnum then
+          if k == nested_key
+            all_values.merge!({"#{path.join('.')}" => "#{v}"})
+          end
+          path.pop
+        when Array then
+          v.each_with_index do |item, i|
+            path << "#{i}" # add the array key
+            keypaths_for_nested_key(nested_key, item, path, all_values)
+          end
+          path.pop # remove the array key
+        when Hash then keypaths_for_nested_key(nested_key, v, path, all_values)
+        else raise ArgumentError, "Unhandled type #{v.class}"
+        end
+      end
+      path.pop
+   
+      return all_values
+    end
+  end
+end
+
+class Hash
+  include KeyPath::HashExtensions
+end
---
+++ spec/hash_extensions_spec.rb
@@ -0,0 +1,47 @@
+# test helpers
+require File.expand_path 'spec_helper.rb', __dir__
+
+describe 'HashExtensions' do
+  it 'adds methods to the Hash class' do
+    hash = {:id => 1}
+
+    hash.must_respond_to 'keypaths_for_nested_key'
+  end
+
+  it 'can find keys in a nested hash' do
+    data = {
+      :id => 1,
+      :item => {
+        :id => 2,
+        :name => 'an item'
+      }
+    }
+    
+    keypaths = data.keypaths_for_nested_key(:id)
+
+    keypaths.wont_be_nil
+    keypaths.must_include 'id'
+    keypaths.must_include 'item.id'
+  end
+
+  it 'can find keys in nested array' do
+    data = {
+      :id => 1,
+      :items => [{
+        :id => 2,
+        :name => 'an item'
+      },
+      {
+        :id => 3,
+        :name => 'another item'
+      }]
+    }
+
+    keypaths = data.keypaths_for_nested_key(:id)
+
+    keypaths.wont_be_nil
+    keypaths.must_include 'id'
+    keypaths.must_include 'items.0.id'
+    keypaths.must_include 'items.1.id'
+  end
+end
