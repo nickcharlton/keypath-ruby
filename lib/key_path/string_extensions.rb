2e2c69de34974c01e5773736e4a65245e3824f5e
Nick Charlton <hello@nickcharlton.net>
2013-12-18 09:46:23 +0000

    Moves the string extensions to a module mixin.

--- lib/key_path/string_extensions.rb
+++ lib/key_path/string_extensions.rb
@@ -1,5 +1,13 @@
-class String
-  def to_keypath
-    KeyPath::Path.new self
+module KeyPath
+  module String
+    module KeyPathExtensions
+      def to_keypath
+        KeyPath::Path.new self
+      end
+    end
   end
+end
+
+class String
+  include KeyPath::String::KeyPathExtensions
 end
