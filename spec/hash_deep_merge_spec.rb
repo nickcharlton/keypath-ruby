efcf3419f0eac2f0b4a00cfb6149494e1a635c2d
Nick Charlton <hello@nickcharlton.net>
2013-12-19 10:38:38 +0000

    Adds specs for the standard merge behaviour.

--- spec/hash_deep_merge_spec.rb
+++ spec/hash_deep_merge_spec.rb
@@ -31,3 +31,28 @@ describe 'HashDeepMerge' do
     one.must_equal three
   end
 end
+
+describe 'HashNormalMerge' do
+  it 'combines nested hashes without decending into them' do
+    one = {'a' => 100, 'b' => 200, 'c' => {'c1' => 12, 'c2' => 14}}
+    two = {'b' => 254, 'c' => 300, 'c' => {'c1' => 16, 'c3' => 94}}
+    expected = {'a' => 100, 'b' => 254, 'c' => {'c1' => 16, 'c3' => 94}}
+
+    output = one.merge(two)
+
+    output.must_be_kind_of Hash
+    output.must_equal expected
+  end
+
+  it 'combines nested hashes without decending into them in place' do
+    one = {'a' => 100, 'b' => 200, 'c' => {'c1' => 12, 'c2' => 14}}
+    two = {'b' => 254, 'c' => 300, 'c' => {'c1' => 16, 'c3' => 94}}
+    expected = {'a' => 100, 'b' => 254, 'c' => {'c1' => 16, 'c3' => 94}}
+
+    one.merge!(two)
+
+    one.must_be_kind_of Hash
+    one.must_equal expected
+  end
+
+end
