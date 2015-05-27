620c28ab152a97fee18f3072e980193dfb7da3a0
Zhang Yuhan <yzhang@yzhang-mbp113.edmunds.hq>
2015-05-27 12:51:46 -0700

    Added support for wildcard hashkey match for deletion
    Added spec for delete_at_path
    
    Adds a method to delete by keypath
    
    to delete items in each element of array in case path includes an array, but no integer index is given
    
    change back the version
    
    removed the comment in the change log

--- lib/key_path/enumerable/extensions.rb
+++ lib/key_path/enumerable/extensions.rb
@@ -51,4 +51,43 @@ module Enumerable
     # merge the new collection into self
     self[key] = collection
   end
+
+  def delete_at_keypath_with_wildcard(keypath)
+    return delete_at_keypath(keypath, true)
+  end
+
+  def delete_at_keypath(keypath, wild_card_allowed = false)
+    keypath = keypath.to_s if keypath.is_a?(KeyPath::Path)
+
+    parts = keypath.split '.', 2
+    return nil if parts.empty?
+
+    index = Integer parts[0] rescue nil
+    unless parts[1]
+      if index
+        return self.delete_at index
+      else
+        return (self.delete parts[0]) || (self.delete parts[0].to_sym)
+      end
+      return
+    end
+
+    # if it's an array, call the index
+    if self.is_a? Array
+      match = index ? self[index] : nil
+    elsif wild_card_allowed && parts[0] == '*'
+      raise "wildcard should only be applied to hash keys; (for array, please skip the key)" unless self.instance_of?(Hash)
+      match = self.values
+    else
+      match = self[parts[0]] || self[parts[0].to_sym]
+    end
+
+    return nil unless match.instance_of?(Hash) || match.instance_of?(Array)
+    if match.instance_of?(Array) and (false if Integer((parts[1].split '.',2).first) rescue true)
+       return match.collect {|m| m.delete_at_keypath(parts[1], wild_card_allowed)}
+    end
+
+    return match.delete_at_keypath(parts[1], wild_card_allowed)
+  end
+
 end
